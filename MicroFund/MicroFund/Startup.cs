cd3200523982ae11b6f6ec7aa5cb2a5e7b334339
Jeff <gradnerj@gmail.com>
2020-10-27 14:43:34 -0600

    Updated EFC packages and AppDbContext namespace references

--- MicroFund/MicroFund/Startup.cs
+++ MicroFund/MicroFund/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MicroFund.Data;
+using DataAccessLayer.Data;
 
 namespace MicroFund {
     public class Startup {
