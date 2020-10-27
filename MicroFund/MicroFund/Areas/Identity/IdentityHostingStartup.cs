15babf1e1c1573d5600e54bc0217450c3f7cb562
Jeff <gradnerj@gmail.com>
2020-10-27 14:52:42 -0600

    Scaffolded in Identity Pages

---
+++ MicroFund/MicroFund/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DataAccessLayer.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(MicroFund.Areas.Identity.IdentityHostingStartup))]
+namespace MicroFund.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
