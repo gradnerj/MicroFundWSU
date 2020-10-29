bc0bb77231e6d63a81ec97a0d074df14d487492e
Luke <45085503+lukemunk@users.noreply.github.com>
2020-10-28 19:09:56 -0600

    Added Utility project with class StaticDetails with constant strings for different roles.

---
+++ MicroFund/Utility/StaticDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility
+{
+    public class StaticDetails
+    {
+        public const string ApplicantRole = "Applicant";
+        public const string MentorRole = "Mentor";
+        public const string JudgeRole = "Judge";
+        public const string AdminRole = "Admin";
+    }
+}
