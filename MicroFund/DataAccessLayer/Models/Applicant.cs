6cb9082914041ee91aa8cbaae8c2abee12037597
Christopherwright2 <52305229+Christopherwright2@users.noreply.github.com>
2020-10-28 18:28:43 -0600

    Adding Applicant Model

---
+++ MicroFund/DataAccessLayer/Models/Applicant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Models
+{
+    public class Applicant
+    {
+        public int Id { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string FullName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string CellPhone { get; set; }
+    }
+}
