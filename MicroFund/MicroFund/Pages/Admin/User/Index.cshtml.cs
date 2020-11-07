59d17f104fca5610a706060deb01bb9aa4753495
Jeff <gradnerj@gmail.com>
2020-11-07 15:02:51 -0700

    Added lockout functionality

--- MicroFund/MicroFund/Pages/Admin/User/Index.cshtml.cs
+++ MicroFund/MicroFund/Pages/Admin/User/Index.cshtml.cs
@@ -7,14 +7,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using DataAccessLayer.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace MicroFund.Pages.Admin.User {
     public class IndexModel : PageModel {
         private readonly IRepository _repository;
+        private readonly ApplicationDbContext _context;
         public IList<IdentityUser> ApplicationUsers { get; set; } 
         public Dictionary<string, string> UserRoles { get; set; }
-        public IndexModel(IRepository repository) {
+
+        public IndexModel(IRepository repository, ApplicationDbContext context) {
             _repository = repository;
+            _context = context;
         }
 
         public async Task OnGetAsync()
@@ -22,6 +27,20 @@ namespace MicroFund.Pages.Admin.User {
             UserRoles = new Dictionary<string, string>();
             ApplicationUsers = await _repository.GetAllUsersAsync();
             UserRoles = await _repository.GetAllUserRolesAsync();
+        }
+
+        public async Task<IActionResult> OnPostLockUnlock(string id) {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if( user.LockoutEnd == null) {
+                user.LockoutEnd = DateTime.Now.AddYears(100);
+            } else if (user.LockoutEnd > DateTime.Now) {
+                user.LockoutEnd = DateTime.Now;
+            } else {
+                user.LockoutEnd = DateTime.Now.AddYears(100);
+            }
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+            return RedirectToPage();
         }
     }
 }
