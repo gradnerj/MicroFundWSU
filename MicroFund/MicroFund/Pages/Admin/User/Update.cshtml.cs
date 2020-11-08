f68fe5f480092d511e88d2e92d5ca20b74810899
Jeff <gradnerj@gmail.com>
2020-11-08 12:53:25 -0700

    Enabled update user role type

--- MicroFund/MicroFund/Pages/Admin/User/Update.cshtml.cs
+++ MicroFund/MicroFund/Pages/Admin/User/Update.cshtml.cs
@@ -4,7 +4,7 @@ using DataAccessLayer.Repository;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MicroFund.Pages.Admin.User {
@@ -12,13 +12,18 @@ namespace MicroFund.Pages.Admin.User {
     {
         private ApplicationDbContext _context;
         private IRepository _repo;
-        public UpdateModel(ApplicationDbContext context, IRepository repo) {
+        private UserManager<IdentityUser> _userManager;
+        public UpdateModel(ApplicationDbContext context, IRepository repo, UserManager<IdentityUser> userManager) {
             _context = context;
             _repo = repo;
+            _userManager = userManager;
         }
         [BindProperty]
         public ApplicationUser AppUser { get; set; }
+        [BindProperty]
         public string UserRole { get; set; }
+
+
         public async Task OnGetAsync(string id)
         {
             AppUser =  _repo.GetUserById(id);
@@ -27,6 +32,13 @@ namespace MicroFund.Pages.Admin.User {
 
         public async Task<IActionResult> OnPostAsync() {
             _context.ApplicationUsers.Update(AppUser);
+            
+            var oldRole = await _userManager.GetRolesAsync(AppUser);
+            if(oldRole.FirstOrDefault() != UserRole) {
+                await _userManager.RemoveFromRoleAsync(AppUser, oldRole.FirstOrDefault());
+                await _userManager.AddToRoleAsync(AppUser, UserRole);
+            }
+            
             await _context.SaveChangesAsync();
             return RedirectToPage("./Index");
         }
