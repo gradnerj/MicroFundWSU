a742013be21fe221c877f73b5853d3875d5d19f0
Montana Throne <mthrone.ii@gmail.com>
2020-11-08 14:42:02 -0700

    Add MSTest project + reference to DataAccessLayer

---
+++ MicroFund/MicroFundTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MicroFundTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
