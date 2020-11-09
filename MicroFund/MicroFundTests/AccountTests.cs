74a018d43ea5af1dc804c08cf1f7941c5d5451a2
Montana Throne <mthrone.ii@gmail.com>
2020-11-08 20:32:23 -0700

    Add Logo to Shared Layout

---
+++ MicroFund/MicroFundTests/AccountTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataAccessLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices.ComTypes;
+using DataAccessLayer.Data;
+
+namespace MicroFundTests
+{
+    [TestClass]
+    public class AccountTests
+    {
+        [TestMethod]
+        public void DuplicateEmails()
+        {
+            //var context = new ApplicationDbContext();
+            //var sUserID = "965103f5-5f7f-4b75-8a87-4af678f1259e";
+        }
+    }
+}
