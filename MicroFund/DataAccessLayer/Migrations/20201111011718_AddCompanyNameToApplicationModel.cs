0d4a76dfd12cf3af4fd1489f0cd908a9dffb809c
Montana Throne <mthrone.ii@gmail.com>
2020-11-10 18:17:53 -0700

    Add CompanyName attribute to Application model

---
+++ MicroFund/DataAccessLayer/Migrations/20201111011718_AddCompanyNameToApplicationModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class AddCompanyNameToApplicationModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyName",
+                table: "Application",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompanyName",
+                table: "Application");
+        }
+    }
+}
