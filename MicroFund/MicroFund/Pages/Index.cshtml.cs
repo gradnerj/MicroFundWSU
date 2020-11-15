7be04397b2c8b1e9150ba6558e48a49c4156f222
Jeff <gradnerj@gmail.com>
2020-11-15 13:09:29 -0700

    Updated Index so Admin gets directed to Calendar

--- MicroFund/MicroFund/Pages/Index.cshtml.cs
+++ MicroFund/MicroFund/Pages/Index.cshtml.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace MicroFund.Pages {
     public class IndexModel : PageModel {
         private readonly ILogger<IndexModel> _logger;
-
+        
         public IndexModel(ILogger<IndexModel> logger) {
             _logger = logger;
+            
         }
 
         public IActionResult OnGet()
@@ -26,6 +23,10 @@ namespace MicroFund.Pages {
                 }
                 else if (User.IsInRole(Utility.StaticDetails.JudgeRole)) {
                     return RedirectToPage("/Judge/Dashboard/Index");
+                }
+                else if (User.IsInRole(Utility.StaticDetails.AdminRole)) {
+                   
+                    return RedirectToPage("/Calendar");
                 }
                 else
                 {
