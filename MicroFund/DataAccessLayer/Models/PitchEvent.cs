42b2a50bfc00bf51d63e80b9d3e496a19d3219df
Christopherwright2 <52305229+Christopherwright2@users.noreply.github.com>
2020-11-15 15:43:36 -0700

    Changed naming on PitchEvent PK and added FK to Pitch table

--- MicroFund/DataAccessLayer/Models/PitchEvent.cs
+++ MicroFund/DataAccessLayer/Models/PitchEvent.cs
@@ -3,8 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace DataAccessLayer.Models {
     public class PitchEvent {
-        [Key]
-        public int PitchId { get; set; }
+        public int PitchEventId { get; set; }
         public DateTime PitchDate { get; set; }
         public float Cash { get; set; }
         public float Services { get; set; }
