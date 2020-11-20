7fd21d56726fabb268bef0fbd4fd19a3e6151b7e
Jeff <gradnerj@gmail.com>
2020-11-19 18:18:51 -0700

    Added var for logging

--- MicroFund/Utility/StaticDetails.cs
+++ MicroFund/Utility/StaticDetails.cs
@@ -10,5 +10,7 @@ namespace Utility
         public const string MentorRole = "Mentor";
         public const string JudgeRole = "Judge";
         public const string AdminRole = "Admin";
+        // For Logging
+        public const string Log = "gradnerj@gmail.com";
     }
 }
