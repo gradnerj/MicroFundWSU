649c30818afe7b34922606b213253ebecc0dfb22
Jeff <gradnerj@gmail.com>
2020-12-23 11:46:46 -0700

    removed api keys

--- MicroFund/DataAccessLayer/Models/AuthSenderOptions.cs
+++ MicroFund/DataAccessLayer/Models/AuthSenderOptions.cs
@@ -9,7 +9,7 @@ namespace DataAccessLayer.Models
 
         private string user = "WSU Microfund Email Verification";
 
-        private string key = "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";
+        private string key = "";
 
         public string SendGridUser
         {
